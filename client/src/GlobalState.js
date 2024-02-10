8904a4a89482cff9b36933f70d9427c24ed9def5
Amarius Jones <amariusj@gmail.com>
2024-02-10 15:54:32 -0500

    Update GlobalState.js
    - Import UseState, UseEffect, Axios, and the UserAPI function
    
    - Uses UseState to create a variable that'll store the access token provided by the API
    
    - Uses UseEffect to check if there's a local storage item labelled 'isLogged'. This item is created whenever a user successfully verifies their credentials with the VerifySMS component. If the item is found, an attempt to grab the refresh token is made. If successful, the provided access token gets stored in the token state
    
    - Creates an object called state which stores the token and userAPI functionality
    
    - Provides the state to the Global Provider

--- client/src/GlobalState.js
+++ client/src/GlobalState.js
@@ -1,7 +1,15 @@
 
 // IMPORTS REACT AND ITS UTILITIES
 
-import React, { createContext } from 'react'
+import React, { createContext, useState, useEffect } from 'react'
+
+// IMPORT AXIOS FOR HTTPS REQUESTS
+
+import axios from 'axios'
+
+// IMPORT THE APIS TO THE CLIENT
+
+import UserAPI from './api/UserAPI'
 
 // CREATES THE CONTEXT THE APP WILL USE
 
@@ -11,10 +19,51 @@ export const GlobalState = createContext()
 
 export function DataProvider({children}) {
 
-// RETURN THE CONTEXT WITHIN THE REACT FUNCTIONAL COMPONENT
+    // CREATE A STATE VARIABLE TO HOLD THE ACCESS TOKEN TO THE API
+
+    const [token, setToken] = useState(false)
+
+    // GRAB THE REFRESH TOKEN WHENEVER A USER LOGS IN
+
+    useEffect(()=> {
+
+        // IF YOU'RE ABLE TO FIND AN ISLOGGED LOCAL STORAGE KEY
+
+        if (localStorage.getItem('isLogged')) {
+
+            // ATTEMPT GRABBING THE REFRESH TOKEN
+
+            const refresh = async () => {
+
+                // MAKE THE REQUEST TO THE API
+
+                const res = await axios.get('user/refresh-token')
+
+                // SET THE TOKEN STATE TO THE ACCESS TOKEN PROVIDED
+
+                setToken(res.data.accessToken)
+
+            }
+
+            // LOG THE REQUEST TO THE CLIENT
+
+            refresh()
+
+        }
+
+    }, [])
+
+    // CREATE A STATE TO HOLD ALL DATA
+
+    const state = useState({
+        token: [token, setToken],
+        userApi: UserAPI(token)
+    })
+
+    // RETURN THE CONTEXT WITHIN THE REACT FUNCTIONAL COMPONENT
 
     return (
-        <GlobalState.Provider value={null} >
+        <GlobalState.Provider value={state} >
             {children}
         </GlobalState.Provider>
     )
