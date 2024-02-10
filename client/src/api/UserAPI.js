590bad4ef7f5c70a7286cd8db711af45177844e5
Amarius Jones <amariusj@gmail.com>
2024-02-10 15:49:54 -0500

    Added UserAPI function
    - Stores all HTTP requests made using the Axios library
    
    - Exports the function and imports it inside GlobalState so that all components have access to it
    
    - Includes a function to get the user's information if an access token is present
    
    - Includes the register, login, and verifySms functions, each making requests to the API

---
+++ client/src/api/UserAPI.js
@@ -0,0 +1,114 @@
+
+// IMPORT USE EFFECT FROM REACT
+
+import { useState, useEffect } from 'react'
+
+// IMPORT AXIOS FOR HTTP REQUESTS
+
+import axios from 'axios'
+
+// CREATE THE REACT FUNCTIONAL COMPONENT
+
+export default function UserAPI(token) {
+
+    // CREATE A STATE VARIABLE TO HOLD THE USER INFORMATION
+
+    const [user, setUser] = useState({})
+
+    // CREATE A USE EFFECT FOR GETTING THE USER'S INFO
+
+    useEffect(() => {
+
+        // IF THERE IS AN ACCESS TOKEN, USE IT TO FIND THE USER
+
+        if (token) {
+
+            // CREATE THE FUNCTION TO GET THE USER INFOR
+
+            const getUser = async () => {
+
+                try {
+
+                    // MAKE THE REQUEST TO THE API
+
+                    const res = await axios.get('/user/infor', {
+                        headers: {
+                            Authorization: token
+                        }
+                    })
+
+                    // STORE THE USER INFORMATION
+
+                    setUser(res.data.user)
+
+                } catch (err) {
+
+                    // IF THERE'S AN ERROR, LOG TO THE CONSOLE AND ALERT THE CLIENT
+
+                    console.log(err)
+
+                    alert(err.response.data.msg)
+
+                }
+
+            }
+
+            // CALL THE GETUSER FUNCTION
+
+            getUser()
+
+        }
+
+    },[token])
+
+    // CREATE A FUNCTION TO REGISTER A NEW USER
+
+    const register =  (user) => {
+
+        // MAKE A REQUEST TO THE API'S REGISTER ROUTE
+
+        const res =  axios.post('/user/register', user)
+
+        // RETURN THE RESPONSE
+
+        return res
+
+    }
+
+    // CREATE A FUNCTION TO VERIFY THE USER CREDENTIALS
+
+    const verifySms = (credentials) => {
+
+        // MAKE A REQUEST TO THE APO'S VERIFY SMS ROUTE
+
+        const res = axios.post('/user/verify-sms', credentials)
+
+        // RETURN THE RESPONSE
+
+        return res
+
+    }
+
+    // CREATE A FUNCTION TO LOG IN AN EXISTING USER
+
+    const login = (user) => {
+
+        // MAKE A REQUEST TO THE API'S REGISTER ROUTE
+
+        const res =  axios.post('/user/login', user)
+
+        // RETURN THE RESPONSE
+
+        return res
+
+    }
+
+    // RETURN THE FUNCTIONS AND STATE TO THE APP
+
+    return {
+        register,
+        verifySms,
+        login,
+        user: [user, setUser]
+    }
+}
